dde6c8023b33836a196538376080952a60c4f3f2
evaveskova <evaveskova@gmail.com>
2020-01-15 17:53:04 +0100

    change function to create todo form and add function to create new project form

--- src/create-form.js
+++ src/create-form.js
@@ -1,35 +1,57 @@
-const createForm = (() => {
-  const form = document.getElementById('create-form');
-  const inputForm = document.createElement('FORM');
-  form.appendChild(inputForm);
+const form = (() => {
 
-  const titleInput = document.createElement('INPUT');
-  inputForm.appendChild(titleInput);
-  titleInput.setAttribute('name', 'title')
+  const newProject = () => {
+    const createForm = document.getElementById('create-form');
+    const projectTitleInputForm = document.createElement('FORM');
+    createForm.appendChild(projectTitleInputForm);
 
-  const descriptionInput = document.createElement('INPUT');
-  inputForm.appendChild(descriptionInput);
-  descriptionInput.setAttribute('name', 'description')
+    const projectTitleInput = document.createElement('INPUT');
+    projectTitleInput.setAttribute('name', 'project-title');
+    projectTitleInputForm.appendChild(projectTitleInput);
 
-  const dateCreatedInput = document.createElement('INPUT');
-  inputForm.appendChild(dateCreatedInput);
-  dateCreatedInput.setAttribute('name', 'date-created')
+    const newProjectBtn = document.createElement('BUTTON');
+    newProjectBtn.setAttribute('type', 'submit');
+    projectTitleInputForm.appendChild(newProjectBtn);
+  };
 
-  const dueDateInput = document.createElement('INPUT');
-  inputForm.appendChild(dueDateInput);
-  dueDateInput.setAttribute('name', 'due-date')
+  const newToDo = () => {
+    const createForm = document.getElementById('create-form');
+    const inputForm = document.createElement('FORM');
+    createForm.appendChild(inputForm);
 
-  const priorityInput = document.createElement('INPUT');
-  inputForm.appendChild(priorityInput);
-  priorityInput.setAttribute('type', 'radio');
-  })
-});
+    const titleInput = document.createElement('INPUT');
+    inputForm.appendChild(titleInput);
+    titleInput.setAttribute('name', 'title');
 
-const popUpForm = (() => {
-  const projectBtn = document.getElementById('new-project');
-  projectBtn.addEventListener('click', () => {
-    form.classList.remove('hidden');
-    form.classList.add('visible');
-    createForm();
-  })
-});
+    const descriptionInput = document.createElement('INPUT');
+    inputForm.appendChild(descriptionInput);
+    descriptionInput.setAttribute('name', 'description');
+
+    const dateCreatedInput = document.createElement('INPUT');
+    inputForm.appendChild(dateCreatedInput);
+    dateCreatedInput.setAttribute('name', 'date-created');
+
+    const dueDateInput = document.createElement('INPUT');
+    inputForm.appendChild(dueDateInput);
+    dueDateInput.setAttribute('name', 'due-date');
+
+    const priorityInput = document.createElement('INPUT');
+    inputForm.appendChild(priorityInput);
+    priorityInput.setAttribute('type', 'radio');
+    }
+  });
+
+  const popUpForm = () => {
+    const projectBtn = document.getElementById('new-project');
+    const createForm = document.getElementById('create-form');
+    projectBtn.addEventListener('click', () => {
+      createForm.classList.remove('hidden');
+      createForm.classList.add('visible');
+      newProject()
+      ();
+    }
+  });
+  return { newProject, newToDo, popUpForm };
+})();
+
+export default form;
