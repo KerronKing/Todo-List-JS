eb572500ed4d661f74edd3e851f3a2589279997e
KerronKing <king_of_palmiste@hotmail.com>
2020-01-16 15:42:54 -0400

    Refactor index logic and projects array definition

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import form from './create-form';
-import projectMethods from './project';
+import { projectMethods, projectsArray } from './project';
 import './styles.css';
 
-console.log(projectMethods.projectsArray);
 form.popUp();
-projectMethods.render(projectMethods.projectsArray);
-
-const projectArea = document.getElementById('projects');
-if (projectArea.innerHTML != '') {
-  projectMethods.projectSubmission();
-}
+projectMethods.projectSubmission();
+console.log(projectsArray);
+projectMethods.render(projectsArray);
--- src/project.js
+++ src/project.js
@@ -1,17 +1,17 @@
 import form from './create-form';
+const projectsArray = [{ title: 'Sample-Project-1' }];
 
 const projectMethods = (() => {
   // const projectsArray = window.localStorage.getItem('projects')
   //   ? JSON.parse(window.localStorage.getItem('projects')) : [];
   //
   // window.localStorage.setItem('projects', JSON.stringify(projectsArray));
-  const projectsArray = [{ title: 'Sample-Project' }];
 
   const Project = (title) => ({ title });
 
   const addToArray = (title) => {
     projectsArray.unshift(Project(title));
-    // window.localStorage.setItem('projects', JSON.stringify(projectsArray));
+    window.localStorage.setItem('projects', JSON.stringify(projectsArray));
   };
 
   const render = (array) => {
@@ -19,7 +19,6 @@ const projectMethods = (() => {
       const projectArea = document.getElementById('projects');
       const div = document.createElement('DIV');
       div.classList.add('project-entry');
-      projectArea.appendChild(div);
 
       const title = document.createElement('P');
       title.textContent = `${item.title}`;
@@ -34,6 +33,7 @@ const projectMethods = (() => {
       deleteBtn.classList.add('delete-entry');
       deleteBtn.textContent = 'Delete';
       div.appendChild(deleteBtn);
+      projectArea.appendChild(div);
     })
   };
   const projectSubmission = () => {
@@ -44,8 +44,6 @@ const projectMethods = (() => {
     projectForm.addEventListener('submit', (e) => {
       e.preventDefault();
       const data = Object.fromEntries(new FormData(projectForm));
-      console.log(`${data.title}`);
-      console.log(data);
       addToArray(data.title);
       createForm.classList.remove('visible');
       createForm.classList.add('hidden');
@@ -53,6 +51,6 @@ const projectMethods = (() => {
       projectForm.reset();
     });
   };
-  return { projectsArray, render, projectSubmission };
+  return { render, projectSubmission };
 })();
-export default projectMethods;
+export { projectMethods, projectsArray };
